fb8d16f4ff2d229e0278afa3f9440233a6def354
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2023-01-30 14:47:44 +0800

    Perf: Doom Emacs config.

--- home/programs/emacs.nix
+++ home/programs/emacs.nix
@@ -1,11 +1,27 @@
 {
+  lib,
   pkgs,
   nix-doom-emacs,
   ...
 }: {
-  programs.doom-emacs = {
+  programs.doom-emacs = rec {
     enable = true;
-    doomPrivateDir = ../config/doom.d; # Directory containing your config.el, init.el
-    # and packages.el files
+    doomPrivateDir = ../config/doom.d;
+
+    # Only init/packages so we only rebuild when those change.
+    doomPackageDir = pkgs.linkFarm "doom-packages-dir" [
+      {
+        name = "init.el";
+        path = ../config/doom.d/init.el;
+      }
+      {
+        name = "packages.el";
+        path = ../config/doom.d/packages.el;
+      }
+      {
+        name = "config.el";
+        path = pkgs.emptyFile;
+      }
+    ];
   };
 }
