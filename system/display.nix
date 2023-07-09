f48fabec025f123c11a7c5bc87f9eaf9076edd19
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2023-07-09 16:52:10 +0800

    Perf: More efficient way to handle greetd.

--- system/display.nix
+++ system/display.nix
@@ -7,6 +7,13 @@
   # pkgs: greetd, tuigreet
   systemd.services.greetd.serviceConfig = {
     Type = "idle";
+    # StandardInput = "tty";
+    # StandardOutput = "tty";
+    # StandardError = "journal"; # Without this errors will spam on screen
+    # Without these bootlogs will spam on screen
+    # TTYReset = true;
+    # TTYVHangup = true;
+    # TTYVTDisallocate = true;
   };
 
   # greetd session manager to fit sway & Wayland.
