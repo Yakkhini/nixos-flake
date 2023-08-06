41fd3d5f8b73ed2991d60f77c7436519223a08f6
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2023-08-06 15:28:44 +0800

    Perf: MirrorZ binary substituters.

--- host/default.nix
+++ host/default.nix
@@ -8,6 +8,7 @@
   ...
 }: {
   nix.settings.substituters = [
+    "https://mirrors.cernet.edu.cn/nix-channels/store"
     "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
     "https://mirrors.ustc.edu.cn/nix-channels/store"
   ];
