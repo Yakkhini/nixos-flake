45398b147fd6e5acaf2b100f729337ba15922803
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-02-12 10:02:34 +0800

    Feat: Close channels.

--- host/default.nix
+++ host/default.nix
@@ -7,6 +7,8 @@
   nur,
   ...
 }: {
+  nix.channel.enable = false;
+
   nix.settings.substituters = [
     "https://mirrors.cernet.edu.cn/nix-channels/store"
     "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
