8cba3e55e94fa89190a6246ba61f780e06486327
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-02-12 10:55:06 +0800

    init wsl.

--- flake.nix
+++ flake.nix
@@ -4,6 +4,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     nur.url = "github:nix-community/NUR";
+    wsl = {
+      url = "github:nix-community/NixOS-WSL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -19,6 +23,7 @@
     nixpkgs,
     home-manager,
     nur,
+    wsl,
     hyprwm-contrib,
     ...
   } @ inputs: {
@@ -34,6 +39,17 @@
         home-manager.nixosModule
         nur.nixosModules.nur
         ./hosts/dell-desktop
+      ];
+    };
+
+    nixosConfigurations.yaksis-thinkbook-wsl = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = inputs;
+      modules = [
+        home-manager.nixosModule
+        nur.nixosModules.nur
+        wsl.nixosModules.wsl
+        ./hosts/thinkbook-wsl
       ];
     };
   };
