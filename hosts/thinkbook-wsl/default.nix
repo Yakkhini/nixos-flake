bb9288bcae32bbf6b7a1781c37cc144fc746c1e1
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-02-26 08:58:08 +0000

    Feat(thinkbook-wsl): Disable windows binaries & path to improve speed.

--- hosts/thinkbook-wsl/default.nix
+++ hosts/thinkbook-wsl/default.nix
@@ -30,6 +30,11 @@
   wsl.defaultUser = "yakkhini";
   wsl.startMenuLaunchers = true;
 
+  wsl.wslConf.interop = {
+    enabled = false;
+    appendWindowsPath = false;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
