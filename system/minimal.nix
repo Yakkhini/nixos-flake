0bbeee421f79c45fb4bb61bdc9591bf52f7506d7
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-02-26 13:58:47 +0000

    Feat: basic i18n support in system minimal configuration.

--- system/minimal.nix
+++ system/minimal.nix
@@ -8,8 +8,11 @@
   imports = [
     ./users.nix
     ./network.nix
-    # ./i18n.nix
     ./fonts.nix
     ./pkgs.nix
   ];
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {LANGUAGE = "en_US.UTF-8";};
+  i18n.supportedLocales = ["en_US.UTF-8/UTF-8" "zh_CN.UTF-8/UTF-8"];
 }
