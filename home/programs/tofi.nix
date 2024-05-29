888d104f8d7c7e7f4dd260e697005a37bd65d4b3
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-05-29 21:19:33 +0800

    Perf: Fullscreen tofi theme.

--- home/programs/tofi.nix
+++ home/programs/tofi.nix
@@ -1,3 +1,15 @@
 {...}: {
   programs.tofi.enable = true;
+  programs.tofi.settings = {
+    background-color = "#000A";
+    border-width = 0;
+    font = "monospace";
+    height = "100%";
+    num-results = 5;
+    outline-width = 0;
+    padding-left = "35%";
+    padding-top = "35%";
+    result-spacing = 25;
+    width = "100%";
+  };
 }
