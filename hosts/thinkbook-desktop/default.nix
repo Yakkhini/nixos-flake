e9451a5dcd6c9e4f8a780f83a233b45970ac1ed4
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2025-03-11 18:05:23 +0800

    [host/thinkbook-desktop] use more stable substituters
    Cernet sometimes too slow.

--- hosts/thinkbook-desktop/default.nix
+++ hosts/thinkbook-desktop/default.nix
@@ -28,8 +28,9 @@
   nix.channel.enable = false;
 
   nix.settings.substituters = [
-    "https://mirrors.cernet.edu.cn/nix-channels/store"
+    "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
     "https://mirrors.ustc.edu.cn/nix-channels/store"
+    "https://mirror.sjtu.edu.cn/nix-channels/store"
   ];
   nix.settings.experimental-features = ["nix-command" "flakes"];
   nix.settings.trusted-users = ["root" "@wheel"];
