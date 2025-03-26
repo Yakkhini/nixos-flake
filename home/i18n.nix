11c609cff101d728777e7ee1d9001985a192a12a
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2025-03-26 15:12:18 +0800

    [home] fcitx5 wayland frontend
    Although I don't this change could do what.

--- home/i18n.nix
+++ home/i18n.nix
@@ -1,10 +1,13 @@
 {pkgs, ...}: {
   i18n.inputMethod = {
     enabled = "fcitx5";
-    fcitx5.addons = with pkgs; [
-      fcitx5-rime
-      fcitx5-chinese-addons
-      fcitx5-gtk
-    ];
+    fcitx5 = {
+      addons = with pkgs; [
+        fcitx5-rime
+        fcitx5-chinese-addons
+        fcitx5-gtk
+      ];
+      waylandFrontend = true;
+    };
   };
 }
