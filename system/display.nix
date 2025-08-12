70cad2b99ad905bec85817beadd9694154d1491a
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2025-08-12 19:15:40 +0800

    [system] remove useless greetd configs

--- system/display.nix
+++ system/display.nix
@@ -7,19 +7,11 @@
   # pkgs: greetd, tuigreet
   systemd.services.greetd.serviceConfig = {
     Type = "idle";
-    # StandardInput = "tty";
-    # StandardOutput = "tty";
-    # StandardError = "journal"; # Without this errors will spam on screen
-    # Without these bootlogs will spam on screen
-    # TTYReset = true;
-    # TTYVHangup = true;
-    # TTYVTDisallocate = true;
   };
 
   # greetd session manager to fit sway & Wayland.
   services.greetd = {
     enable = true;
-    vt = 2;
     settings = {
       default_session = {
         command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --time --cmd niri-session";
