8459563f0a6dbd09d421bea6a34681e05fe81d2b
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2025-10-24 20:59:46 +0800

    [system] remove unused clash-meta program since clash verge installed

--- system/clash.nix
+++ system/clash.nix
@@ -1,22 +1,4 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
-  # Clash deamon service
-  # systemd.services.clash-daemon = {
-  #   enable = true;
-  #   description = "Clash daemon, A rule-based proxy in Go.";
-  #   after = ["network.target"];
-  #   serviceConfig = {
-  #     Type = "simple";
-  #     Restart = "always";
-  #     ExecStart = "${pkgs.clash-meta}/bin/clash-meta -d /home/yakkhini/.config/clash";
-  #   };
-  #   wantedBy = ["multi-user.target"];
-  # };
-
+{...}: {
   programs.clash-verge = {
     enable = true;
     autoStart = true;
