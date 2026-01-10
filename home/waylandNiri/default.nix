77e81d0152b99600001c88e3d628ed58d5f62f54
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2026-01-10 22:53:34 +0800

    [home] enable swayidle & hyprlock as idle manager

--- home/waylandNiri/default.nix
+++ home/waylandNiri/default.nix
@@ -27,6 +27,47 @@
     };
   };
 
+  # Ref: https://wiki.nixos.org/wiki/Swayidle
+  services.swayidle = let
+    # Lock command
+    lock = "${pkgs.niri}/bin/niri msg action spawn -- hyprlock";
+    # TODO: modify "display" function based on your window manager
+    # Sway
+    # display = status: "${pkgs.sway}/bin/swaymsg 'output * power ${status}'";
+    # Hyprland
+    # display = status: "hyprctl dispatch dpms ${status}";
+    # Niri
+    display = status: "${pkgs.niri}/bin/niri msg action power-${status}-monitors";
+  in {
+    enable = true;
+    timeouts = [
+      {
+        timeout = 595; # in seconds
+        command = "${pkgs.libnotify}/bin/notify-send 'Locking in 5 seconds' -t 5000";
+      }
+      {
+        timeout = 600;
+        command = lock;
+      }
+      {
+        timeout = 1200;
+        command = display "off";
+        resumeCommand = display "on";
+      }
+      {
+        timeout = 1500;
+        command = "${pkgs.systemd}/bin/systemctl suspend";
+      }
+    ];
+    events = {
+      # adding duplicated entries for the same event may not work
+      before-sleep = (display "off") + "; " + lock;
+      after-resume = display "on";
+      lock = (display "off") + "; " + lock;
+      unlock = display "on";
+    };
+  };
+
   programs.wlogout.enable = true;
 
   xsession.windowManager.i3.enable = true;
