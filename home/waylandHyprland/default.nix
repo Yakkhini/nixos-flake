93028063d54f5a871e463a3550e9dcad9ecb951c
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2026-01-10 22:52:05 +0800

    [home] move wayland configs to `waylandNiri` config

--- home/waylandHyprland/default.nix
+++ home/waylandHyprland/default.nix
@@ -5,39 +5,6 @@
     extraConfig = import ./hyprland.conf;
   };
 
-  services.swww.enable = true;
-
   xdg.configFile."hypr/hyprlock.conf".source = ./hyprlock.conf;
   xdg.configFile."hypr/hypridle.conf".source = ./hypridle.conf;
-
-  programs.wlogout.enable = true;
-
-  catppuccin.mako.enable = true;
-  services.mako = {
-    enable = true;
-
-    settings = {
-      actions = true;
-      anchor = "bottom-center";
-      default-timeout = 5000;
-
-      height = 300;
-      width = 500;
-
-      font = "monospace 12";
-      border-radius = 10;
-      border-size = 2;
-      padding = "10";
-
-      "actionable=true" = {
-        anchor = "bottom-right";
-      };
-    };
-  };
-
-  xsession.windowManager.i3.enable = true;
-
-  xresources.properties = {
-    "Xft.dpi" = 192;
-  };
 }
--- home/waylandNiri/default.nix
+++ home/waylandNiri/default.nix
@@ -1,4 +1,37 @@
 {pkgs, ...}: {
   programs.niri.enable = true;
   programs.niri.package = pkgs.niri;
+
+  services.swww.enable = true;
+  catppuccin.mako.enable = true;
+
+  services.mako = {
+    enable = true;
+
+    settings = {
+      actions = true;
+      anchor = "bottom-center";
+      default-timeout = 5000;
+
+      height = 300;
+      width = 500;
+
+      font = "monospace 12";
+      border-radius = 10;
+      border-size = 2;
+      padding = "10";
+
+      "actionable=true" = {
+        anchor = "bottom-right";
+      };
+    };
+  };
+
+  programs.wlogout.enable = true;
+
+  xsession.windowManager.i3.enable = true;
+
+  xresources.properties = {
+    "Xft.dpi" = 192;
+  };
 }
