bd0f025a76caf8b9b4a45558f676431ad3d455ea
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2026-04-20 16:45:59 +0800

    [system] specify mounting disk file system type to auto
    See upstream change details:
    https://github.com/NixOS/nixpkgs/commit/f8ed5f30c0d34714c4e82b221e083a83c0e5f0f5

--- hosts/thinkbook-desktop/mount.nix
+++ hosts/thinkbook-desktop/mount.nix
@@ -1,18 +1,15 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
+{...}: {
   # Mount Disks
   fileSystems."/mnt/c" = {
     device = "/dev/nvme0n1p3";
+    fsType = "auto";
     noCheck = true;
     options = ["rw" "noatime"];
   };
 
   fileSystems."/mnt/d" = {
     device = "/dev/nvme0n1p5";
+    fsType = "auto";
     noCheck = true;
     options = ["uid=yakkhini" "gid=users" "rw" "noatime"];
   };
