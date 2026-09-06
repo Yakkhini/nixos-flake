b5222696c0dfcb7583aa007196ec2bf6b991c067
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2026-09-06 14:29:56 +0800

    [update] 20260906 update

--- flake.nix
+++ flake.nix
@@ -31,10 +31,6 @@
       url = "github:sodiboo/niri-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    noctalia = {
-      url = "github:noctalia-dev/noctalia-shell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     zen-browser = {
       url = "github:youwen5/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
